7b4a07bcdd89783f3d3adf1bfd466b90276e6d7b
janani-ruwandharani <70389230+janani-ruwandharani@users.noreply.github.com>
2021-02-25 17:07:55 +0530

    Update server.js

--- server/src/server.js
+++ server/src/server.js
@@ -6,40 +6,11 @@ if (process.env.NODE_ENV !== 'production') {
 
 var app = require('./app');
 
-
-
-
-var express = require("express");
-var path = require("path");
-var bodyParser = require("body-parser");
-var mongodb = require("mongodb");
-var ObjectID = mongodb.ObjectID;
-
-//var CONTACTS_COLLECTION = "contacts";
-
-//var app = express();
-//app.use(express.static(__dirname + "/public"));
-//app.use(bodyParser.json());
-
-// Create a database variable outside of the database connection callback to reuse the connection pool in your app.
-var db;
-
-// Connect to the database before starting the application server.
-mongodb.MongoClient.connect(process.env.MONGODB_URI, function (err, database) {
-  if (err) {
-    console.log(err);
-    process.exit(1);
-  }
-  // Save database object from the callback for reuse.
-  db = database;
-  console.log("Database connection ready");
-
   // Initialize the app.
   var port = process.env.PORT || 5000;
   var server = app.listen(port, function () {
   console.log('Express server listening on port ' + port);
 });
   
-});
 
-// CONTACTS API ROUTES BELOW
+
